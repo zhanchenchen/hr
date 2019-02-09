5530f12fc390c17471d1cb3cbc9a151ea6b4ad92
zcc <540490410@qq.com>
2019-02-09 13:33:00 +0800

    a little change in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
 	public static void main(String args[]){
 		System.out.print("Hello World!");
-		System.out.print("Hello World!");
+		System.out.print("fool me");
+		System.out.print("fool me");
 	}
 }
