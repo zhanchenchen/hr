354a135841b53918d795ec219fdf8b1c65dc1401
zcc <540490410@qq.com>
2019-02-09 14:00:35 +0800

    delete Hello.java

--- Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-	public static void main(String args[]){
-		System.out.print("Hello World!");
-		System.out.print("fool me");
-		System.out.print("fool me");
-		System.out.print("fool me");
-	}
-}
