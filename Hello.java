ac255e064862d4647d4d1e360dba04476fb8cf61
zcc <540490410@qq.com>
2019-02-09 17:34:41 +0800

    add

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String args[]){
+		System.out.print("Hello World!");
+		System.out.print("fool me");
+		System.out.print("fool me");
+		System.out.print("love me");
+	}
+}
